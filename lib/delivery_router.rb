1fda419b3ca4e838f43c1c14043ef4d3b0267fad
David <eth3rnit3@gmail.com>
2022-10-08 23:21:13 +0200

    start router

--- lib/delivery_router.rb
+++ lib/delivery_router.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
 class DeliveryRouter
+  attr_reader :restaurants, :customers, :riders
+
+  def initialize(restaurants, customers, riders)
+    @restaurants  = restaurants
+    @customers    = customers
+    @riders       = riders
+  end
+
+  def add_order(customer: nil, restaurant: nil)
+    raise "Can't add order if customer and/or restaurant is nil" if customer.nil? || restaurant.nil?
+
+    valid_customer    = customer_from_params!(customer)
+    valid_restaurant  = restaurant_from_params!(restaurant)
+
+    valid_customer.add_order(valid_restaurant)
+  end
+
+  private
+
+  def customer_from_params!(customer_id)
+    customer = @customers.find { |cust| cust.id == customer_id }
+    raise "Customer not found with id #{customer_id}" if customer.nil?
+
+    customer
+  end
+
+  def restaurant_from_params!(restaurant_id)
+    restaurant = @restaurants.find { |cust| cust.id == restaurant_id }
+    raise "Restaurant not found with id #{restaurant_id}" if restaurant.nil?
+
+    restaurant
+  end
 end
--- models/customer.rb
+++ models/customer.rb
@@ -2,4 +2,18 @@ require_relative './base'
 
 class Customer < ModelBase
   ATTRIBUTES = %i[].freeze
+
+  def initialize(attributes)
+    super(attributes)
+
+    @orders = []
+  end
+
+  def add_order(restaurant)
+    @orders << { restaurant: restaurant, order_at: Time.now }
+  end
+
+  def cancel_order(restaurant)
+    @orders = @orders.reject { |order| order[:restaurant].id == restaurant.id }
+  end
 end
